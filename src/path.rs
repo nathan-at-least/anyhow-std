ca766cf94b57d2cb30a4e4f001263403a935f76f
Nate Wilcox <nathan+dev@electriccoin.co>
2023-03-29 21:22:28 -0700

    Factor out `PathAnyhow` wrapper structure w/ a macro.

--- src/path.rs
+++ src/path.rs
@@ -8,30 +8,36 @@ pub trait PathAnyhow {
     fn file_name_anyhow(&self) -> anyhow::Result<&OsStr>;
 }
 
+macro_rules! wrap_nullary_option_method {
+    ( $method:ident, $cb:expr, $ret:ty, $errordesc:expr ) => {
+        fn $method(&self) -> anyhow::Result<$ret> {
+            let p = self.as_ref();
+            $cb(p)
+                .ok_or_else(|| anyhow::Error::msg($errordesc))
+                .with_context(|| format!("while processing path {:?}", p.display()))
+        }
+    };
+}
+
 impl<P> PathAnyhow for P
 where
     P: AsRef<Path>,
 {
-    fn to_str_anyhow(&self) -> anyhow::Result<&str> {
-        let p = self.as_ref();
-        p.to_str()
-            .ok_or_else(|| anyhow::Error::msg("invalid UTF8"))
-            .with_context(|| format!("while processing path {:?}", p.display()))
-    }
+    wrap_nullary_option_method!(to_str_anyhow, Path::to_str, &str, "invalid UTF8");
 
-    fn parent_anyhow(&self) -> anyhow::Result<&Path> {
-        let p = self.as_ref();
-        p.parent()
-            .ok_or_else(|| anyhow::Error::msg("expected parent directory"))
-            .with_context(|| format!("while processing path {:?}", p.display()))
-    }
+    wrap_nullary_option_method!(
+        parent_anyhow,
+        Path::parent,
+        &Path,
+        "expected parent directory"
+    );
 
-    fn file_name_anyhow(&self) -> anyhow::Result<&OsStr> {
-        let p = self.as_ref();
-        p.file_name()
-            .ok_or_else(|| anyhow::Error::msg("missing expected filename"))
-            .with_context(|| format!("while processing path {:?}", p.display()))
-    }
+    wrap_nullary_option_method!(
+        file_name_anyhow,
+        Path::file_name,
+        &OsStr,
+        "missing expected filename"
+    );
 }
 
 #[cfg(test)]
