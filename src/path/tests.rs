b0778ac2a69a3b9b9a2871ca711e9d49e17fc502
Nate Wilcox <nathan+dev@electriccoin.co>
2023-03-29 21:30:21 -0700

    Factor out error description assertions in `Path` tests.

--- src/path/tests.rs
+++ src/path/tests.rs
@@ -16,17 +16,15 @@ fn to_str_invalid_utf8() -> anyhow::Result<()> {
     use std::os::unix::ffi::OsStrExt;
 
     let path = Path::new(OsStr::from_bytes(b"\x81\xff"));
-    let error = format!("{:?}", path.to_str_anyhow().err().unwrap());
-    assert_eq!(
-        error,
+    assert_error_desc_eq(
+        path.to_str_anyhow(),
         indoc! { r#"
             while processing path "\x81\xFF"
 
             Caused by:
                 invalid UTF8
         "#
-        }
-        .trim_end()
+        },
     );
     Ok(())
 }
@@ -42,17 +40,15 @@ fn parent_non_root() -> anyhow::Result<()> {
 #[test]
 fn parent_root() -> anyhow::Result<()> {
     let path = Path::new("/");
-    let error = format!("{:?}", path.parent_anyhow().err().unwrap());
-    assert_eq!(
-        error,
+    assert_error_desc_eq(
+        path.parent_anyhow(),
         indoc! { r#"
             while processing path "/"
 
             Caused by:
                 expected parent directory
         "#
-        }
-        .trim_end()
+        },
     );
     Ok(())
 }
@@ -67,17 +63,20 @@ fn file_name_present() -> anyhow::Result<()> {
 #[test]
 fn file_name_missing() -> anyhow::Result<()> {
     let path = Path::new("/foo/..");
-    let error = format!("{:?}", path.file_name_anyhow().err().unwrap());
-    assert_eq!(
-        error,
+    assert_error_desc_eq(
+        path.file_name_anyhow(),
         indoc! { r#"
             while processing path "/foo/.."
 
             Caused by:
                 missing expected filename
         "#
-        }
-        .trim_end()
+        },
     );
     Ok(())
+}
+
+fn assert_error_desc_eq<T>(res: anyhow::Result<T>, expected: &str) {
+    let error = format!("{:?}", res.err().unwrap());
+    assert_eq!(error, expected.trim_end());
 }
