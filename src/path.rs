2d886072bad80c641073323431f66706d3a03b92
Nate Wilcox <nathan+dev@electriccoin.co>
2023-03-29 21:02:11 -0700

    Add `Path::to_str_anyhow`.

--- src/path.rs
+++ src/path.rs
@@ -3,6 +3,7 @@ use std::ffi::OsStr;
 use std::path::Path;
 
 pub trait PathAnyhow {
+    fn to_str_anyhow(&self) -> anyhow::Result<&str>;
     fn file_name_anyhow(&self) -> anyhow::Result<&OsStr>;
 }
 
@@ -10,6 +11,13 @@ impl<P> PathAnyhow for P
 where
     P: AsRef<Path>,
 {
+    fn to_str_anyhow(&self) -> anyhow::Result<&str> {
+        let p = self.as_ref();
+        p.to_str()
+            .ok_or_else(|| anyhow::Error::msg("invalid UTF8"))
+            .with_context(|| format!("while processing path {:?}", p.display()))
+    }
+
     fn file_name_anyhow(&self) -> anyhow::Result<&OsStr> {
         let p = self.as_ref();
         p.file_name()
--- src/path/tests.rs
+++ src/path/tests.rs
@@ -3,6 +3,35 @@ use indoc::indoc;
 use std::path::Path;
 
 #[test]
+fn to_str_utf8() -> anyhow::Result<()> {
+    let path = Path::new("/foo/bar.txt");
+    assert_eq!("/foo/bar.txt", path.to_str_anyhow()?);
+    Ok(())
+}
+
+#[cfg(target_family = "unix")]
+#[test]
+fn to_str_invalid_utf8() -> anyhow::Result<()> {
+    use std::ffi::OsStr;
+    use std::os::unix::ffi::OsStrExt;
+
+    let path = Path::new(OsStr::from_bytes(b"\x81\xff"));
+    let error = format!("{:?}", path.to_str_anyhow().err().unwrap());
+    assert_eq!(
+        error,
+        indoc! { r#"
+            while processing path "\x81\xFF"
+
+            Caused by:
+                invalid UTF8
+        "#
+        }
+        .trim_end()
+    );
+    Ok(())
+}
+
+#[test]
 fn file_name_present() -> anyhow::Result<()> {
     let path = Path::new("/foo/bar.txt");
     assert_eq!("bar.txt", path.file_name_anyhow()?);
